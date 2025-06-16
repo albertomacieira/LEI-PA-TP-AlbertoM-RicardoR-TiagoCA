183733d203988701ad64a9b326cc0ea134a61124
Alberto Macieira <albertoomacieira@gmail.com>
2025-06-16 18:41:19 +0100

    Test1

---
+++ Ticket2Help/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ticket2Help
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
